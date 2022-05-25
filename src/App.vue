eb6f7e7aa1068d92ed1cae784208cbc9b5be068d
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-05-25 04:03:33 +0300

    feat: добавить фильтр

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
+<div class="container"></div>
   <FormAdd/>
   <AppTitle/>
+  <AppFilter/>
 </template>
 
 <script>
 import FormAdd from './components/FormAdd/FormAdd.vue'
 import AppTitle from './components/AppTitle/AppTitle.vue'
+import AppFilter from './components/AppFilter/AppFilter.vue'
 
 export default {
   name: 'App',
   components: {
     FormAdd,
     AppTitle,
+    AppFilter
 }
 }
 </script>
 
 <style lang="scss">
+@import './components/constants/colors';
+
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+}
+
+body {
+  background-color: $bg;
 }
 
 #app {
@@ -30,5 +40,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  max-width: 1140px;
+  margin: 0 auto;
+  padding: 0 15px;
 }
 </style>
---
+++ src/components/AppFilter/AppFilter.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="filter">
+  <button class="filter__button filter__button_important active">Важные</button>
+  <button class="filter__button filter__button_process active">В процессе</button>
+  <button class="filter__button filter__button_done active">Выполненные</button>
+</div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import './AppFilter'
+</style> 
