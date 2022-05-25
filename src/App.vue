7a726690e57856330089b7560e3bb8eb3aa13797
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-05-25 03:28:32 +0300

    feat: добавить форму добавления задачь

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <form-add/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FormAdd from './components/FormAdd/FormAdd.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FormAdd
   }
 }
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/FormAdd/FormAdd.vue
@@ -0,0 +1,17 @@
+<template>
+  <form class="form-add" @submit.prevent>
+      <input class="form-add__input" type="text">
+      <button class="form-add__important-button active" type="button"></button>
+      <button class="form-add__add-button"></button>
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+@import './FormAdd'
+</style>
